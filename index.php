f5228e3a28c1778fd7f5ca8a7ec7edca103fba32
Ivo Azirjans <ivo.azirjans@vvsoftmedia.com>
2013-11-08 11:49:20 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => 'compilation_cache',
+));
+
+echo $twig->render('pages/homepage.html.twig');
+
